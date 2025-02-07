3dd85de759df24b540f844476ee7bd0389f9dfa6
Carcuor22 <xccueort046@ieshnosmachado.org>
2025-02-07 10:28:19 +0100

    menu y manifets

--- __manifest__.py
+++ __manifest__.py
@@ -1,9 +1,7 @@
-# __manifest__.py
-
 {
-    'name': 'Taller Mecánico',
+    'name': 'Gestión de Taller Mecánico',
     'version': '1.0',
-    'summary': 'Gestión de taller mecánico: vehículos, reparaciones y mecánicos',
+    'summary': 'Gestión de taller mecánico: clientes, vehículos, reparaciones y mecánicos',
     'description': """
         Módulo de Odoo para gestionar un taller mecánico:
         - Gestión de clientes y vehículos
@@ -14,18 +12,32 @@
     'author': 'Tu Nombre o Grupo',
     'website': 'https://www.example.com',
     'category': 'Industries',
-    'depends': ['base'],  # Dependencia de los módulos básicos de Odoo
+    'depends': ['base'],  # Dependencias necesarias para el módulo
+
     'data': [
         'security/ir.model.access.csv',
+
+        # Vistas principales
         'views/persona_view.xml',
         'views/mecanico_view.xml',
         'views/cliente_views.xml',
+        'views/vehiculo_form_view.xml',
+        'views/reparacion_views.xml',
+        'views/lineareparacion_views.xml',
+        'views/concepto_views.xml',
+        'views/menu_views.xml',
+
+        # Datos de demostración
         'data/demo_data.xml',
+
+        # Informes
         'reports/informe.xml',
     ],
+
     'demo': [
         'data/demo_data.xml',
     ],
+
     'installable': True,
     'application': True,
     'auto_install': False,
