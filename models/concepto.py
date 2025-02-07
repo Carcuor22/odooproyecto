7a4c89421640206c6413e0c22054fd30430ed2ac
Alvaro060 <xadazcru320@ieshnosmachado.org>
2025-02-07 13:37:12 +0100

    .

--- models/concepto.py
+++ models/concepto.py
@@ -4,7 +4,7 @@ class Concepto(models.Model):
     _name = 'odooproyecto.concepto'
     _description = 'Conceptos de Reparación'
 
-    descripcion = fields.Char(string="Descripción", required=True)
+    descripcion = fields.Text(string="Descripción", required=True)
     precio = fields.Float(string="Precio", required=True)
     mano_obra = fields.Float(string="Mano de Obra", required=True)
     reparacion_id = fields.Many2one('odooproyecto.reparacion', string="Reparación", required=True, ondelete='cascade')
