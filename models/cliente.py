67f08c108f9090bd93cd94c70a167c6b1ccd62b3
Carcuor22 <xccueort046@ieshnosmachado.org>
2025-02-10 18:23:01 +0100

    .

--- models/cliente.py
+++ models/cliente.py
@@ -3,9 +3,11 @@ from odoo import models, fields
 class Cliente(models.Model):
     _name = 'taller.cliente'
     _description = 'Clientes del taller'
-    _inherits = {'taller.persona': 'persona_id'}  # ← Usamos persona_id en lugar de nif
+    _inherits = {'taller.persona': 'persona_id'} 
+    _inherit = 'taller.cliente' # ← Usamos persona_id en lugar de nif
 
     persona_id = fields.Many2one('taller.persona', required=True, ondelete='cascade')  # ← Cambiado a Many2one
+     vehiculos_ids = fields.One2many('taller.vehiculo', 'cliente_id', string='Vehículos')
     tipo = fields.Selection([
         ('empresa', 'Empresa'),
         ('particular', 'Particular'),
